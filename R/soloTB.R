27a27bfa8ca485baa2768137f5625aea4b291393
gisma <gisma@gis-ma.org>
2017-10-11 09:02:32 +0200

    add documentation

--- R/soloTB.R
+++ R/soloTB.R
@@ -3,18 +3,20 @@ if (!isGeneric('solo_upload')) {
   setGeneric('solo_upload', function(x, ...)
     standardGeneric('solo_upload'))
 }
-#' upload mission file to solo
-#'
+#' Upload MAV compliant mission File to a 3DR Solo
 #' @description  solo_upload provides a crude interface to upload the Solo mission file to the 3dr SOLO
 #'
 #' @param connection a valid connection string to the Solo default is "udp:10.1.1.166:14550"
-#' @param prearm controls the prearm status of the Solo prearm check 0=Disabled,1=Enabled,-3=Skip Baro,-5=Skip Compass,-9=Skip GPS,-17=Skip INS,-33=Skip Params/Rangefinder,-65=Skip RC,127=Skip Voltage 
+#' @param prearm \code{character} controls the prearm status of the Solo prearm check\cr 0=Disabled\cr 1=Enabled\cr -3=Skip Baro\cr -5=Skip Compass\cr -9=Skip GPS\cr -17=Skip INS\cr -33=Skip Params/Rangefinder\cr -65=Skip RC\cr 127=Skip Voltage\cr
+#' default is \code{-1}\cr\cr Find more information at \href{http://ardupilot.org/copter/docs/prearm_safety_check.html}{prearm safety},\cr \href{http://python.dronekit.io/examples/mission_import_export.html}{Mission import export script}.
+#' 
+#'  
 #' @param missionFile mission file to upload
 #' 
 #'
 #'
 #'
-#'@note for using the solo stuff you need to install: \cr sudo pip install pymavlink;\cr sudo pip install dronekit-sitl;\cr sudo pip install dronekit; \cr sudo apt-get install sshpass\cr Additionally you need to be connected to a running 3DR Solo uav 
+#'@note Becareful with fooling around with the prearm stuff. It is kind of VERY sensitive for the later autonomous flights!\cr For using the Solo stuff you need to install: \cr sudo pip install pymavlink;\cr sudo pip install dronekit-sitl;\cr sudo pip install dronekit; \cr sudo apt-get install sshpass\cr Additionally you need to be connected to a running 3DR Solo uav 
 #'
 #' @examples
 #' wp <- system.file("extdata", "MAVLINK_waypoints.txt", package = "uavRmp")
@@ -26,7 +28,7 @@ if (!isGeneric('solo_upload')) {
 
 solo_upload <- function(missionFile = NULL, 
                         connection = "udp:10.1.1.166:14550", 
-                        prearm = "-9"){
+                        prearm = "-1"){
   
   command <- 'python'
   script <- paste(system.file(package="uavRmp"), "python/io_solo_mission.py", sep = "/")
@@ -54,26 +56,28 @@ if (!isGeneric('soloLog')) {
   setGeneric('soloLog', function(x, ...)
     standardGeneric('soloLog'))
 }
-#' Download, reorganize and export the log files from 3DR Solo and radio control unit
+#' Download, reorganize and export the binary log files from 3DR Solo Pixhawk controller or the telemetry log files from the Solo radio control unit
 #'
-#' @description  Wraps the mavtogpx.py converter as provided by the dronkit library. It downloads and/ or converts the 3DR Solo logfiles. Otionally you may import the geometries and data as sp objects in R
+#' @description  Wraps the mavtogpx.py converter as provided by the \href{http://python.dronekit.io/}{dronkit library}). It downloads and optionally converts the most important 3DR Solo logfiles. Optionally you may import the geometries and data as \code{sp} object.
 #'
-#' @param logSource \code{rc} = logfiles from the radio control, \code{pixhawk} = logfiles from the flightcontroller, default is \code{rc}. The radio control is providing the last ten telemetry data files, while the flight controller provides the latest 50 binary logfiles.
+#' @param logSource \code{character}, options are: \code{rc} = logfiles from the radio control, \code{pixhawk} = logfiles from the flightcontroller, default is set to  \code{rc}. The radio control is providing the last ten telemetry data files, while the flight controller provides the latest 50 binary logfiles.
 
-#' @param logFiles If logSource = "pixhawk" the argument "recent" will download the most recent logfile from the pixhawk, other options are "all" for all logfiles or a plain number i.e. "1" for a specific one.
-#' @param logDir (existing) destination path to which the logs should be downloaded to 
-#' @param downloadOnly default = TRUE, set to FALSE  if you want to convert the log files from the solo remote controller to GPX files
-#' @param netWarn if true warns and waits before starting a connection to the controller to connect to the solo wifi
-#' @param organize renames the log and gpx files according to their timeperiod
+#' @param logFiles \code{character} , options are:  \code{recent} download the most recent logfile,  \code{all} downloads all logfiles, or a plain number e.g. \code{2} for a specific logfile. Note the telemetry logfiles are numbering from 1 to 9 only, the most recent one is not numbered. The binary logfiles from the pixhawk are numbering continously but only the last 50 files or so will exist. 
 #' 
-#' @param makeSP generates SP objects from the gpx files
+#' @param logDir \code{character} (existing) destination path to which the logs should be downloaded to 
+#' @param downloadOnly \code{logical} wether to only download the files or also convert and rename them, default is set FALSE
+#' @param netWarn \code{logical} wether to warn and waits before starting a connection to the controller. helps while testing due to occassional wifi shutdowns of the Solo, default is set to FALSE
+#' @param renameFiles  \code{logical} renames the log and gpx files according to the time period, default is set TRUE
 #' 
-#' @note for using the solo stuff you need to install: \cr 
-#' sudo pip install pymavlink  \cr 
-#' sudo pip install dronekit-sitl \cr 
-#' sudo pip install dronekit \cr 
-#' sudo apt-get install sshpass \cr 
-#' \cr Additionally you need to be connected to a running 3DR Solo uav 
+#' @param makeSP \code{logical} wether returning an \code{sp} object from the gpx files or not, default is FALSE
+#' 
+#' @note for using the Solo stuff is tested only for Linux and the bash shell under Windows 10. You need to install the following python libs: \cr 
+#' \code{sudo pip install pymavlink}  \cr 
+#' \code{sudo pip install dronekit-sitl} \cr 
+#' \code{sudo pip install dronekit} \cr \cr
+#' Additionally you need \code{sshpass}:\cr
+#' \code{sudo apt-get install sshpass} \cr 
+#' \cr And please rememeber - you need to be connected at least to a running 3DR Solo radio control and if you want to donload data from the Pixhawk to a Solo UAV
 #' 
 #' @examples
 #' \dontrun{
@@ -100,13 +104,13 @@ soloLog <- function(logFiles="recent",
                     logSource="rc",
                     logDir=tmpDir(), 
                     downloadOnly=FALSE,
-                    netWarn=TRUE,
-                    organize=TRUE,
+                    netWarn=FALSE,
+                    renameFiles=TRUE,
                     makeSP = FALSE){
   exit<-NULL
   logDir<- path.expand(logDir)
   command <-"mavtogpx.py"
- # option1<-paste0(logDir,"/",logFiles)
+  # option1<-paste0(logDir,"/",logFiles)
   
   if (!file.exists(file.path(logDir))) {
     dir.create(file.path(logDir), recursive = TRUE)
@@ -117,12 +121,12 @@ soloLog <- function(logFiles="recent",
   cat("downloading and converting will take a while without prompting anything...\n be patient in the end you will know.\n")
   
   if (logSource == "rc")  {
-    if (logFiles=="all") {logFiles<-"solo.t*"}
+    if (logFiles=="all") {logFiles<-"solo.tlo???"}
     else if (logFiles=="recent") {logFiles<-"solo.tlog"}
     else  logFiles<-paste("solo.tlog.",logFiles)
     option1<-paste0(logDir,"/",logFiles)  
-      cat("you will download the following files:\n")
-      cat(system(paste0("sshpass -p 'TjSDBkAu' ssh  root@10.1.1.1 ls -l -h /log/",logFiles),intern = TRUE))
+    cat("you will download the following files:\n")
+    cat(system(paste0("sshpass -p 'TjSDBkAu' ssh  root@10.1.1.1 ls -l -h /log/",logFiles),intern = TRUE))
     
     log<-system( paste0("sshpass -p 'TjSDBkAu'  scp 'root@10.1.1.1:/log/",logFiles,"' ",logDir,"/. " ),wait = TRUE)
   } else {
@@ -140,61 +144,77 @@ soloLog <- function(logFiles="recent",
       log<-system( paste0("sshpass -p 'TjSDBkAu'  scp -P 22 root@10.1.1.10:/log/dataflash/",logFiles,".BIN ",logDir,"/. " ),wait=TRUE)
     }
   }
-
+  
   if (log == 0) {
     f <- list.files(logDir, pattern=extension(logFiles))
     cat(f," downloaded...\n")
-    cat("Download from solo controllor seems to be ok\n")
+    cat("Download from Solo controllor seems to be ok\n")
   } else {
     cat('FATAL', "### can not find/read input file")        
     stop("### could not read any log data\n")
   }  
+  
   if (downloadOnly){
     cat("All logs downloaded...")
     return()
   } 
-  
-  test<-system2("mavtogpx.py","-h",stdout = TRUE)
-  if (grep("usage: mavtogpx.py",test)){
-    cat("pymavlink seems to be installed\n")
-    cat("converting log files to to gpx...\n")
-    test<-system2(command, option1,stdout = TRUE)
-    cat(test)
-  } else {
-    stop("No pymavlink lib. Try: sudo pip install pymavlink\n")
-  }
+
   
   
-  if (organize) {
-    cat("\nrename files...\n")
-    f <- list.files(logDir, pattern="gpx")
+  if (renameFiles) {
+    # create filelists
+    tlogFiles <- Sys.glob(file.path(logDir, "*.tlog.?"))
+    tlogFiles <- c(Sys.glob(file.path(logDir, "*.tlog")),tlogFiles)
+    gpxLogFiles <- paste0(tlogFiles,".gpx")  
+    test <- system2("mavtogpx.py","-h",stdout = TRUE)
+    if (grep("usage: mavtogpx.py",test)) {
+      cat("at least pymavlink is installed :-)\n")
+      cat("converting log files to to gpx...\n")
+      if (logFiles == "all") {
+        for (log in tlogFiles) {
+          test <- system2(command, log,stdout = TRUE)
+        }
+        
+      } else  test <- system2(command, option1,stdout = TRUE)
+      cat(test)
+    } else {
+      stop("No pymavlink lib. Try: sudo pip install pymavlink\n")
+    }
     
-    i=1
+    cat("\nrename files...\n")
+    i <- 1
     flights <- list()
-    for (flight in f) {
-      f <- read_gpx(path.expand(paste0(logDir,"/",flight)))
-      flights[[i]]<-f
-      firstTime<-as.character(flights[[i]]$track_points@data$time)[1]
-      lastTime<-as.character(flights[[i]]$track_points@data$time)[length(flights[[i]]$track_points@data$time)]
-      la1<-gsub(x = lastTime, pattern = "\\/",replacement = "")
-      la2<-gsub(x = la1, pattern = "\\ ",replacement = "_")
-      la3<-substr(gsub(x = la2, pattern = "\\:",replacement = "-"),10,17)
-      
-      fi1<-gsub(x = firstTime, pattern = "\\/",replacement = "")
-      fi2<-gsub(x = fi1, pattern = "\\ ",replacement = "_")
-      fi3<-substr(gsub(x = fi2, pattern = "\\:",replacement = "-"),1,17)
-      logName<- paste0(logDir,"/",fi3,"_",la3,"_solo.tlog")
-      gpxName<- paste0(logDir,"/",fi3,"_",la3,"_solo.gpx")      
-      fNgpx <- paste0(logDir,"/",list.files(logDir, pattern="gpx"))
-      fNlog <- paste0(logDir,"/",list.files(logDir, pattern="tlog",include.dirs = FALSE))
-      if (!file.exists(logName)) file.rename(fNlog[i],logName)
-      else {cat("you'd already converted ",logName,"\n")}
-      if (!file.exists(gpxName)) file.rename(fNgpx[i],gpxName)
-      else {cat("you'd already converted ",gpxName,"\n")}
-      
-      i=i+1
+    for (flight in gpxLogFiles) {
+      f <- read_gpx(flight)
+      flights[[i]] <- f
+      firstTime <- as.character(flights[[i]]$track_points@data$time)[1]
+      lastTime <- as.character(flights[[i]]$track_points@data$time)[length(flights[[i]]$track_points@data$time)]
+      la1 <- gsub(x = lastTime, pattern = "\\/",replacement = "")
+      la2 <- gsub(x = la1, pattern = "\\ ",replacement = "_")
+      la3 <- substr(gsub(x = la2, pattern = "\\:",replacement = "-"),10,17)
+      Sys.glob("*.dbf")
+      fi1 <- gsub(x = firstTime, pattern = "\\/",replacement = "")
+      fi2 <- gsub(x = fi1, pattern = "\\ ",replacement = "_")
+      fi3 <- substr(gsub(x = fi2, pattern = "\\:",replacement = "-"),1,17)
+      logName <- paste0(logDir,"/",fi3,"_",la3,"_solo.tlog")
+      gpxName <- paste0(logDir,"/",fi3,"_",la3,"_solo.gpx")      
+      #fNgpx <- paste0(logDir,"/",list.files(logDir, pattern="solo.tlog.*.gpx"))
+      #fNlog <- Sys.glob(file.path(logDir, "*.tlog.?"))
+      #fNlog<-c(Sys.glob(file.path(logDir, "*.tlog")),fNlog)
+      if (!file.exists(logName)) {
+        file.rename(tlogFiles[i],logName)
+      } else {
+        cat("you'd already converted ",logName,"\n")
+      }
+      if (!file.exists(gpxName)) {
+        file.rename(gpxLogFiles[i],gpxName)
+      } else {
+        cat("you'd already converted ",gpxName,"\n")
+      }
+      i <- i + 1
     }
     cat("All logfiles stored and coverted ...\n")
+    
     if (makeSP) {
       cat("export as sp objects ...\n")
       return(flights)}
