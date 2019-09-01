f1b52b2c07958606c6d163924d68cac75bc5d12d
gisma <giswerk@gis-ma.org>
2019-09-01 19:21:38 +0200

    exif

--- R/postprocessingTools.R
+++ R/postprocessingTools.R
@@ -1,42 +1,3 @@
-#' systematically delete files from a given directory
-#' @description  #' systematically delete files from a given directory
-#'
-#' @param position default is TRUE the files are selected by their relative position
-#' @param distance default is 30 m distance of the image centres 
-#' @param path path to the images files
-#'
-#' @examples
-#' wp <- system.file("extdata", "MAVLINK_waypoints.txt", package = "uavRmp")
-#' \dontrun{
-#' solo_upload( missionFile = wp)
-#' }
-#' @export drop_images
-#'               
-
-drop_images <- function(path = NULL ,
-                        position = TRUE, 
-                        distance = 30,
-                        image_format = "jpg",
-                        perlcmd= "C:\\Strawberry\\perl\\bin\\perl"){
-
-if (image_format != "jpg" & image_format !="raw") return()
-  
-flist<-list()
-flist<-append(flist, Sys.glob(file.path(R.utils::getAbsolutePath(path),"*.JPG")))
-dat <- exifr::read_exif(path,recursive = TRUE,args = "-n")
-ret2 <- dplyr::select(dat,
-               SourceFile, DateTimeOriginal,
-               GPSLongitude, GPSLatitude,
-               GPSTimeStamp)
-library(geosphere)
-distm(c(lon1, lat1), c(lon2, lat2), fun = distHaversine)
-write.csv(dat2, 'Exifdata.csv',
-          row.names = F)
-exifinfo <- read_exif(path = path,recursive = TRUE,tags = c("gpslatitude", "gpslongitude","gpstimestamp"), args = "-n")
-ret<-system2("C:\\Strawberry\\perl\\bin\\perl","C:\\Users\\solo\\Documents\\R\\win-library\\3.4\\exifr\\exiftool\\exiftool.pl -filename -gpslatitude -gpslongitude -T -n X:\\nature40\\2019_05_16_core14\\images > X:/nature40/2019_05_16_core14/images/out.txt",stdout = TRUE)
-
-return(flist)
-}
 
 #' extract exif information
 #' @description  extract exif
@@ -56,42 +17,56 @@ return(flist)
 drop_images <- function(path = NULL ,
                         position = TRUE, 
                         distance = 30,
-                        image_format = "jpg",
-                        perlcmd= "C:\\Strawberry\\perl\\bin\\perl"){
+                        image_format = "jpg"){
   
   if (image_format != "jpg" & image_format !="raw") return()
   
   if (Sys.info()["sysname"] == "Linux") {
-    perlPath  <- try(system2("find", paste("/usr/bin"," ! -readable -prune -o -type f -executable -iname 'perl' -print"), stdout = TRUE))
-    perlPath<- file.path(R.utils::getAbsolutePath(perlPath),"perl")
-    rappdir_location <- try(system2("find", paste("/usr/bin"," ! -readable -prune -o -type f  -iname 'exiftool.pl' -print"), stdout = TRUE))
+    perlPath  <- try(system2("find", paste("/usr/bin"," ! -readable -prune -o -type f -executable -iname 'exiftool' -print"), stdout = TRUE))
+    perlPath<- file.path(R.utils::getAbsolutePath(perlPath),"exiftool -n")
     
   }
   if (Sys.info()["sysname"] == "Windows") {
-    perlPath <- try(system(paste0("cmd.exe /c dir /B /S ",DL,"\\","perl.exe"),intern = TRUE))
-    perlPath<- file.path(R.utils::getAbsolutePath(perlPath),"perl.exe")
+    print(":: searching for the exiftool... this may take a while")
+    perlPath <- try(system(paste0("cmd.exe /c dir /B /S C:\\","exiftool.exe"),intern = TRUE))
+    perlPath<- shortPathName(file.path(R.utils::getAbsolutePath(perlPath)))
+    perlPath<-paste(perlPath,"-n")
     
   }
-  v
   
-  rappdir_location <- file.path(
-    rappdirs::user_data_dir(appname = "r-exifr", appauthor = "paleolimbot"),
-    "exiftool"
-  )
+  if (length(perlPath) > 0) exiftool<-perlPath[2]
+  else {
+    print("No exiftools installed... pls install and retry")
+    return()
+  }
+  
+  
   
   flist<-list()
+  exifInfos<-list()
+  imageLat<-list()
+  imageLon<-list()
   flist<-append(flist, Sys.glob(file.path(R.utils::getAbsolutePath(path),"*.JPG")))
-  # dat <- exifr::read_exif(path,recursive = TRUE,args = "-n")
-  # ret2 <- dplyr::select(dat,
-  #                SourceFile, DateTimeOriginal,
-  #                GPSLongitude, GPSLatitude,
-  #                GPSTimeStamp)
-  ret<-system2(perlPath, paste("-filename -gpslatitude -gpslongitude -T -n ", path," > ", path,"out.txt"),stdout = TRUE)
+  print(paste("retrieving exif information for ",length(flist), "files this may take a while "))
+  for (j in seq(1:length(flist))) {
+    cat(paste(j,"\b"))
+    
+    #svMisc::progress(j,progress.bar = TRUE)
+    exifInfos[[j]]<-system(paste(exiftool,flist[j]),intern = TRUE)  
+    gpsposRow<-grep("GPS Position",exifInfos[[j]],useBytes = TRUE)
+    imageLat[[j]] <- strsplit(strsplit(exifInfos[[j]][gpsposRow],":")[[1]][2]," ")[[1]][2]
+    imageLon[[j]] <- strsplit(strsplit(exifInfos[[j]][gpsposRow],":")[[1]][2]," ")[[1]][3]
+    if (j==length(flist)) message(done)
+  }
+  
+  cat("done")
+  
+  
   library(geosphere)
   distm(c(lon1, lat1), c(lon2, lat2), fun = distHaversine)
   write.csv(dat2, 'Exifdata.csv',
             row.names = F)
   
   
-  return(flist)
+  return()
 }
