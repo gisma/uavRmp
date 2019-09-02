5efdaccdd2c627b4c766676f0b3522003c6f47fc
gisma <reudenbach@uni-marburg.de>
2019-09-02 12:07:47 +0200

    exif time distance data table

--- R/postprocessingTools.R
+++ R/postprocessingTools.R
@@ -16,15 +16,15 @@
 #'               
 
 selectImages <- function(path = NULL ,
-                        position = TRUE, 
-                        distance = 30,
-                        image_format = "jpg"){
+                         position = TRUE, 
+                         distance = 30,
+                         image_format = "jpg"){
   
   if (image_format != "jpg" & image_format !="raw") return()
   
   if (Sys.info()["sysname"] == "Linux") {
     perlPath  <- try(system2("find", paste("/usr/bin"," ! -readable -prune -o -type f -executable -iname 'exiftool' -print"), stdout = TRUE))
-    perlPath<- file.path(R.utils::getAbsolutePath(perlPath),"exiftool -n")
+    perlPath<- paste0(perlPath," -n")
     
   }
   if (Sys.info()["sysname"] == "Windows") {
@@ -35,7 +35,7 @@ selectImages <- function(path = NULL ,
     
   }
   
-  if (length(perlPath) > 0) exiftool<-perlPath[2]
+  if (length(perlPath) > 0) exiftool<-perlPath[1]
   else {
     print(":: No exiftools installed... pls install and retry")
     return()
@@ -52,27 +52,43 @@ selectImages <- function(path = NULL ,
   flist<-append(flist, Sys.glob(file.path(R.utils::getAbsolutePath(path),"*.JPG")))
   print(paste(":: retrieving exif information from ",length(flist), "files - this may take a while..."))
   fn<-file.path(R.utils::getAbsolutePath(path),paste0(strsplit(path,.Platform$file.sep)[[1]][length(strsplit(path,.Platform$file.sep)[[1]])-1],".csv"))
-  for (j in seq(1:length(flist))) {
-    cat(paste("reading exif from image No.: ",j,"\r"))
-    #svMisc::progress(j,progress.bar = TRUE)
-    exifInfos[[j]]<-system(paste(exiftool,flist[j]),intern = TRUE)  
-    gpsposRow<-grep("GPS Position",exifInfos[[j]],useBytes = TRUE)
-    timeRow<-grep("Modify Date",exifInfos[[j]],useBytes = TRUE)
-    imageDate[j] <- strsplit(strsplit(exifInfos[[j]][timeRow]," :")[[1]][2]," ")[[1]][2]
-    imageTime[j] <- strsplit(strsplit(exifInfos[[j]][timeRow]," :")[[1]][2]," ")[[1]][3]
-    imageLat[j] <- strsplit(strsplit(exifInfos[[j]][gpsposRow],":")[[1]][2]," ")[[1]][2]
-    imageLon[j] <- strsplit(strsplit(exifInfos[[j]][gpsposRow],":")[[1]][2]," ")[[1]][3]
-   # write.csv2(x = exifInfos[[j]],file=fn,append = TRUE)
+  if (file.exists(fn)) {
+    cat(":: file ",fn, "already exists - moved to",paste0(fn,"~\n"))
+    file.rename(from = fn,to = paste0(fn,"~"))
   }
   
-  message("done")
-  write.csv2(x = exifInfos,file=fn)
   
-  # library(geosphere)
-  # distm(c(lon1, lat1), c(lon2, lat2), fun = distHaversine)
-  # write.csv(dat2, 'Exifdata.csv',
-  #           row.names = F)
-  # 
+  exifInfo <- lapply(seq(length(flist)), function(j){
+    cat(paste("::: reading exif from image No.: ",j,"\r"))
+    #svMisc::progress(j,progress.bar = TRUE)
+    img<-unlist(flist[j])
+    exifInfos<-system(paste(exiftool,flist[j]),intern = TRUE)  
+    gpsposRow<-grep("GPS Position",exifInfos,useBytes = TRUE)
+    timeRow<-grep("Modify Date",exifInfos,useBytes = TRUE)
+    warningRow<- grep("Warning",exifInfos,useBytes = TRUE)
+    if (length(warningRow)==0){
+      imageDate <- strsplit(strsplit(exifInfos[timeRow]," :")[[1]][2]," ")[[1]][2]
+      imageTime <- strsplit(strsplit(exifInfos[timeRow]," :")[[1]][2]," ")[[1]][3]
+      imageLat <- strsplit(strsplit(exifInfos[gpsposRow],":")[[1]][2]," ")[[1]][2]
+      imageLon <- strsplit(strsplit(exifInfos[gpsposRow],":")[[1]][2]," ")[[1]][3]
+      write.table(x = as.data.frame(exifInfos),file=fn,append = TRUE,sep = ",",row.names=F, col.names=F )
+      data.frame(img=img,date =  imageDate, time=imageTime,lat=imageLat,lon=imageLon,stringsAsFactors = FALSE)
+    } 
+    else {
+      message(":: image ",flist[j], " has invalid exif data")
+    }
+  })
+  exifInfo <- as.data.frame(do.call("rbind", exifInfo))
+  exifInfo$da<- paste0(exifInfo$date," ",exifInfo$time)
+  exifInfo$time <- strptime(exifInfo$da,format = "%Y:%m:%d %H:%M:%S")
+  exifInfo$lat<- as.numeric(exifInfo$lat)
+  exifInfo$lon<- as.numeric(exifInfo$lon)
   
-  return(list(imageDate,imageTime,imageLat,imageLon))
+  exifInfo$dist[1]<-0
+  exifInfo$timediff[1]<-0
+  for (i in seq(1:nrow(exifInfo))-1)   {
+    exifInfo$dist[i+1]<-geosphere::distm(c(exifInfo$lon[i],exifInfo$lat[i]), c(exifInfo$lon[i+1],exifInfo$lat[i+1]), fun = distGeo)
+    exifInfo$timediff[i+1]<-exifInfo$time[i+1]-exifInfo$time[i]
+  }
+  return(exifInfo)
 }
